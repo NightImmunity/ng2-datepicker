fb8422d9f320a759f32374762e77334049706dd0
Brian Thompson <brianrt23@gmail.com>
2016-09-19 11:19:12 -0500

    added datepicker module

---
+++ ng2-datepicker.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DatePickerComponent} from './src/components/ng2-datepicker';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, BrowserModule],
+  declarations: [DatePickerComponent],
+  exports: [DatePickerComponent],
+  id: 'DPCM',
+  bootstrap: [DatePickerComponent]
+})
+
+export class DatePickerComponentModule {}
